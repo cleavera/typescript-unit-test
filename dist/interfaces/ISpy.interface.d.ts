2cb137b3692a306d40c0e828674b6efc8089e700
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2017-01-26 08:20:11 +0000

    Adding when called with to ISpy interface

--- dist/interfaces/ISpy.interface.d.ts
+++ dist/interfaces/ISpy.interface.d.ts
@@ -3,6 +3,7 @@ export interface ISpy {
     fakeFn: Function;
     retValue: any;
     callFake(fn: Function): ISpy;
+    whenCalledWith(...args: any[]): ISpy;
     reset(): ISpy;
     returnValue(value: any): ISpy;
 }
